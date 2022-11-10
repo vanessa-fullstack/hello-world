0e1c6b419fe8812969dda760fd7997c61f139085
Vanessa <vanessacarney_@hotmail.co.uk>
2022-11-10 12:03:26 +0000

    exploring jsx

--- src/index.js
+++ src/index.js
@@ -5,11 +5,53 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+//VERSION 1 OF HOW TO WRITE THIS CODE USING REACT
+// const p1 = React.createElement("p",{id : "firstPara"} , "This is paragraph 1");
+// const p2 = React.createElement("p",null, "This is paragraph 2");
+
+// const myList = React.createElement("ul", null, [
+//   React.createElement("li",null, "item 1"),
+//   React.createElement("li",null, "item 2"),
+//   React.createElement("li",null, "item 3")
+// ]);
+
+// const mainDiv = React.createElement("div", {name : "myDiv", id : "firstDiv"}, [p1, p2, myList]);
+// root.render(mainDiv);
+
+//VERSION 2 JSX
+// const p1 = <p id="firstPara">This is paragraph 1</p>;
+// const p2 = <p>This is paragraph 2</p>;
+// const myList = <ul>
+//   <li>item 1</li>
+//   <li>item 2</li>
+//   <li>item 3</li>
+// </ul>;
+// const mainDiv = <div>
+//   {p1}{p2}{myList}
+// </div>
+// root.render(mainDiv);
+
+//VERSION 3
+const myName = "Vanessa";
+const mainDiv = <div id="main">
+  <p>This is the first paragraph. Hello {myName}</p>
+  <p>This is the second paragraph</p>
+  <ul>
+    <li>First item</li>
+    <li>Second item</li>
+    <li>Third item</li>
+  </ul>
+  <p>The total of 7 + 9 is {7 + 9}</p>
+</div>
+root.render(mainDiv);
+
+//THIS WAS THE ORIGINIAL CODE WHEN FILE WAS DOWNLOADED FROM CMD
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
